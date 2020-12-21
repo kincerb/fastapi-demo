85f68906da35fcf0e4bcab0c02f0ea5857fa0e43
kincerb <dev.bkincer@gmail.com>
2020-12-20 23:09:46 -0500

    Initial add

---
+++ main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import fastapi
+import uvicorn
+from starlette.staticfiles import StaticFiles
+
+from api import weather_api
+from views import home
+
+api = fastapi.FastAPI()
+
+
+def configure():
+    _configure_routing()
+
+
+def _configure_routing():
+    api.mount("/static", StaticFiles(directory="static"), name="static")
+    api.include_router(home.router)
+    api.include_router(weather_api.router)
+
+
+if __name__ == "__main__":
+    configure()
+    uvicorn.run(api, port=8000, host="127.0.0.1")
+else:
+    configure()
